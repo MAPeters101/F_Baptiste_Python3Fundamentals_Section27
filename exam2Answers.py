83f3a6078cf8d3d924dd69a21655180fef5c0a2d
Mark Peters <mapeters101@earthlink.net>
2025-03-03 21:54:58 -0500

    Answers for Exam 2 reformatted.

--- exam2Answers.py
+++ exam2Answers.py
@@ -1,23 +1,13 @@
 '''
 Exam 2 - Answer Key
-Q1:
-b
-Q2:
-c
-Q3:
-d
-Q4:
-b
-Q5:
-c
-Q6:
-b
-Q7:
-d
-Q8:
-b
-Q9:
-c
-Q10:
-c
+Q1: b
+Q2: c
+Q3: d
+Q4: b
+Q5: c
+Q6: b
+Q7: d
+Q8: b
+Q9: c
+Q10: c
 '''
